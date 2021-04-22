2ecb9816be50b3f7372fd65a01fad7444f5d9c6a
jéricho <56830022+jerichojazz@users.noreply.github.com>
2021-04-22 14:17:09 +0200

    major update: improved performance
    add `@st.cache` decorator
    reorganized layout

--- borea_xplore.py
+++ borea_xplore.py
@@ -3,9 +3,17 @@ import numpy as np
 import pandas as pd
 from io import StringIO
 from datetime import date, timedelta
+
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+
 sns.set()
 
 st.title('BOREA-XPLORE')
@@ -46,16 +54,12 @@ specie2 = st.sidebar.selectbox(
     'Espèce 2',
      top25.index
      )
-specie3 = st.sidebar.selectbox(
+specie_hue = st.sidebar.selectbox(
     'Espèce 3',
      top25.index
      )
-specie_hue = st.sidebar.selectbox(
-    'Espèce 4',
-     top25.index
-     )
 
-st.sidebar.markdown('__Analyse en composantes principales (ACP) + T-SNE__') 
+st.sidebar.markdown('__(ACP), (T-SNE), (LDA)__') 
 
 specie_target = st.sidebar.selectbox(
     'Espèce cible',
@@ -71,11 +75,12 @@ specie_target = st.sidebar.selectbox(
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # histogramme du top25
-
-plt.figure(figsize=(15,6))
-sns.barplot(x=top25.index, y=top25.values)
-plt.xticks(rotation=45)
-st.pyplot()
+def plot_hist1():
+	plt.figure(figsize=(15,6))
+	sns.barplot(x=top25.index, y=top25.values)
+	plt.xticks(rotation=45)
+	st.pyplot()
+plot_hist1()
 
 # data cleaning
 df = df.reset_index()
@@ -83,61 +88,70 @@ df = df.rename(columns={'index':'date'})
 df['date'] = pd.to_datetime(df['date'])
 
 """
-#### Représentation des occurrences au cours des années
+#### Représentation des occurrences au cours des années***
 ---
 """
-'Espèces : ',specie1,specie2,specie3
+'Espèces : ',specie1,specie2,specie_hue
 
-plt.figure(figsize=(15,6))
-sns.lineplot(data=df, x='date', y=specie1, label=specie1)
-sns.lineplot(data=df, x='date', y=specie2, label=specie2)
-sns.lineplot(data=df, x='date', y=specie3, label=specie3)
-
-st.pyplot()
-
-"""
-#### Distribution par espèces
----
-"""
-
-plt.figure(figsize=(20,9))
-for i,var in enumerate([specie1,specie2,specie3]):
-    plt.subplot(2,4,i+1)
-    sns.histplot(data=df, x=var, kde=True)
-    plt.title(var)
-    plt.xlabel('')
-st.pyplot()
-
-"""
-#### Boxplot par espèces
----
-"""
-
-plt.figure(figsize=(20,9))
-for i,var in enumerate([specie1,specie2,specie3]):
-    plt.subplot(2,4,i+1)
-    sns.boxenplot(data=df, y=var)
-    plt.title(var)
-    plt.xlabel('')
-    
-st.pyplot()
+@st.cache(suppress_st_warning=True)
+def plot_figure1():
+	plt.figure(figsize=(15,6))
+	sns.lineplot(data=df, x='date', y=specie1, label=specie1)
+	sns.lineplot(data=df, x='date', y=specie2, label=specie2)
+	sns.lineplot(data=df, x='date', y=specie_hue, label=specie_hue)
+	st.pyplot()
+plot_figure1()
 
 """
 #### Matrice de corrélation entre les 25 espèces les plus représentées
 ---
 """
+@st.cache(suppress_st_warning=True)
+def plot_figure2():
+	plt.figure(figsize=(18,11))
+	sns.heatmap(df.iloc[:,0:24].corr(),annot=True,cmap='viridis')
+	st.pyplot()
+plot_figure2()
 
-plt.figure(figsize=(18,11))
-sns.heatmap(df.iloc[:,0:24].corr(),annot=True,cmap='viridis')
-st.pyplot()
 
 """
-#### Relations entre espèces
+#### Distribution par espèces***
+---
+"""
+
+def plot_figure3():
+	plt.figure(figsize=(20,9))
+	for i,var in enumerate([specie1,specie2,specie_hue]):
+		plt.subplot(2,4,i+1)
+		sns.histplot(data=df, x=var, kde=True)
+		plt.title(var)
+		plt.xlabel('')
+	st.pyplot()
+plot_figure3()
+
+"""
+#### Boxplot par espèces***
+---
+"""
+def plot_figure4():
+	for i,var in enumerate([specie1,specie2,specie_hue]):
+		plt.subplot(2,4,i+1)
+		sns.boxenplot(data=df, y=var)
+		plt.title(var)
+		plt.xlabel('')
+	
+	st.pyplot()
+plot_figure4()
+
+"""
+#### Relations entre espèces***
 --- 
 """
-
-sns.scatterplot(data=top25_df, x=specie1, y=specie2, size=specie3, hue=specie_hue,palette='viridis')
-st.pyplot()
+@st.cache(suppress_st_warning=True)
+def plot_figure5():
+	sns.scatterplot(data=top25_df, x=specie1, y=specie2, hue=specie_hue,palette='viridis')
+	st.pyplot()
+plot_figure5()
 
 """
 ## Analyse par composantes principales
@@ -148,15 +162,12 @@ st.pyplot()
 """
 'Cible : ',specie_target
 
-
-df_ = top25_df.drop(columns=specie_target)
+df_off_target = top25_df.drop(columns=specie_target)
 target = top25_df[specie_target]
 
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-Z = sc.fit_transform(df_)
+Z = sc.fit_transform(df_off_target)
 
-from sklearn.decomposition import PCA
 pca = PCA()
 Coord = pca.fit_transform(Z)
 
@@ -164,82 +175,116 @@ Coord = pca.fit_transform(Z)
 ### Somme cumulative représentant le ratio de la variance expliquée en fonction du nombre de composantes.
 ---
 """
+
 len_pca = len(pca.explained_variance_ratio_.cumsum())
-sns.lineplot(x=range(len_pca),
-				y=pca.explained_variance_ratio_.cumsum(),
-				color='green')
-plt.xticks(range(1,len_pca+1),labels=range(1,len_pca+1))
-st.pyplot()
+
+def plot_figure6():
+	sns.lineplot(x=range(len_pca),
+					y=pca.explained_variance_ratio_.cumsum(),
+					color='green')
+	plt.xticks(range(1,len_pca+1),labels=range(1,len_pca+1))
+	st.pyplot()
+plot_figure6()
 
 """
 ### Camembert la part de variance expliquée par chaque axe de l'ACP
 ---
 """
 
-# data
-pie_data = pca.explained_variance_
-pie_data = np.concatenate((pie_data[:5],pie_data[6:].sum()),axis=None)
-# figure
-labels=['C1','C2','C3','C4','C5','6+']
-explode=(0.1,0,0,0,0,0)
-plt.pie(pie_data,startangle=90,autopct='%1.1f%%',shadow=True,explode=explode)
-plt.legend(loc='best',labels=labels)
-st.pyplot()
-
+def plot_figure7():
+	pie_data = pca.explained_variance_
+	pie_data = np.concatenate((pie_data[:5],pie_data[6:].sum()),axis=None)
+	labels=['C1','C2','C3','C4','C5','6+']
+	explode=(0.1,0,0,0,0,0)
+	plt.pie(pie_data,startangle=90,autopct='%1.1f%%',shadow=True,explode=explode)
+	plt.legend(loc='best',labels=labels)
+	st.pyplot()
+plot_figure7()
 """
 ### Coefficient de corrélation de chaque variable de l'ACP sur les deux premiers axes
 ---
 """
-
-Comp_PCA = pd.DataFrame({'PC1':pca.components_[:,0],'PC2':pca.components_[:,1]})
-sns.heatmap(Comp_PCA, annot=True, cmap='viridis')
-st.pyplot()
+@st.cache(suppress_st_warning=True)
+def plot_figure8():
+	Comp_PCA = pd.DataFrame({'PC1':pca.components_[:,0],'PC2':pca.components_[:,1]})
+	sns.heatmap(Comp_PCA, annot=True, cmap='viridis')
+	st.pyplot()
+plot_figure8()
 
 """
 ### Cercle des corrélations
 ---
 """
-racine_valeurs_propres=np.sqrt(pca.explained_variance_)
-corvar=np.zeros((len_pca,len_pca))
-for k in range(len_pca):
-	corvar[:,k]=pca.components_[:,k]*racine_valeurs_propres[k]
+@st.cache(suppress_st_warning=True)
+def plot_figure9():
+	racine_valeurs_propres=np.sqrt(pca.explained_variance_)
+	corvar=np.zeros((len_pca,len_pca))
+	for k in range(len_pca):
+		corvar[:,k]=pca.components_[:,k]*racine_valeurs_propres[k]
 
-# Délimitation de la figure
-fig,axes=plt.subplots(figsize=(10,10))
-axes.set_xlim(-1,1)
-axes.set_ylim(-1,1)
-# Affichage des variables
-for j in range(len_pca):
-	plt.annotate(top25_df.columns[j],(corvar[j,0],corvar[j,1]),color='#091158')
-	plt.arrow(0,0,corvar[j,0]*0.6,corvar[j,1]*0.6,alpha=0.5,head_width=0.03,color='b')
+	# Délimitation de la figure
+	fig,axes=plt.subplots(figsize=(10,10))
+	axes.set_xlim(-1,1)
+	axes.set_ylim(-1,1)
+	# Affichage des variables
+	for j in range(len_pca):
+		plt.annotate(top25_df.columns[j],(corvar[j,0],corvar[j,1]),color='#091158')
+		plt.arrow(0,0,corvar[j,0]*0.6,corvar[j,1]*0.6,alpha=0.5,head_width=0.03,color='b')
 
-# Ajout des axes
-plt.plot([-1,1],[0,0],color='silver',linestyle='-',linewidth=1)
-plt.plot([0,0],[-1,1],color='silver',linestyle='-',linewidth=1)
+	# Ajout des axes
+	plt.plot([-1,1],[0,0],color='silver',linestyle='-',linewidth=1)
+	plt.plot([0,0],[-1,1],color='silver',linestyle='-',linewidth=1)
 
-# Cercle et légendes
-cercle=plt.Circle((0,0),1,color='#16E4CA',fill=False)
-axes.add_artist(cercle)
-plt.xlabel('AXE1')
-plt.ylabel('AXE2')
+	# Cercle et légendes
+	cercle=plt.Circle((0,0),1,color='#16E4CA',fill=False)
+	axes.add_artist(cercle)
+	plt.xlabel('AXE1')
+	plt.ylabel('AXE2')
 
-# display
-st.pyplot()
-
+	# display
+	st.pyplot()
+plot_figure9()
 """
-### ACP + algorithme T-SNE (t-distributed stochastic neighbor embedding)
+### T-SNE (t-distributed stochastic neighbor embedding)***
 ---
 * représentation des coordonnées dans un plan (AXE_1 / AXE_2) en fonction de l'espèce 'cible'
 """
 'Cible : ',specie_target
 
-from sklearn.manifold import TSNE
-tsne_new = TSNE(n_components=2, random_state=0)
-Coord_TSNE_ACP = tsne_new.fit_transform(Coord)
-tsne_acp_df = pd.DataFrame({'AXE_1':Coord_TSNE_ACP[:,0],
-                           'AXE_2':Coord_TSNE_ACP[:,1],
-                           specie_target:target})
+@st.cache(suppress_st_warning=True)
+def plot_figure10():
 
-sns.scatterplot(data=tsne_acp_df,x='AXE_1',y='AXE_2',hue=specie_target,palette='viridis');
+	tsne_new = TSNE(n_components=2, random_state=0)
+	Coord_TSNE_ACP = tsne_new.fit_transform(Coord)
+	tsne_acp_df = pd.DataFrame({'AXE_1':Coord_TSNE_ACP[:,0],
+							   'AXE_2':Coord_TSNE_ACP[:,1],
+							   specie_target:target})
 
-st.pyplot()
+	sns.scatterplot(data=tsne_acp_df,x='AXE_1',y='AXE_2',hue=specie_target,palette='viridis');
+
+	st.pyplot()
+plot_figure10()
+
+"""
+### LDA (Analyse par Discriminant Linéaire)***
+---
+* représentation des coordonnées dans un plan (AXE_1 / AXE_2) en fonction de l'espèce 'cible'
+"""
+'Cible : ',specie_target
+
+@st.cache(suppress_st_warning=True)
+def plot_figure11():
+	lda = LDA()
+	X_LDA = lda.fit_transform(df_off_target,target)
+
+	lda_df = pd.DataFrame({'AXE_1':X_LDA[:,0],
+							   'AXE_2':X_LDA[:,1],
+							   specie_target:target})
+							   
+	sns.scatterplot(data=lda_df, x='AXE_1',y='AXE_2',hue=specie_target)
+	plt.title('Analyse par Discriminant Linéaire')
+	plt.ylabel('AXE_1')
+	plt.xlabel('AXE_2')
+
+	st.pyplot()
+plot_figure11()
